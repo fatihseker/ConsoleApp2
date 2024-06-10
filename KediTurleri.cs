60295ff54985ff76bd02dddda26058371858a9b0
FATIH <FATIH@DESKTOP-T1ACLC3>
2024-06-10 17:47:43 +0300

    Proje dosyası ekle.

---
+++ KediTurleri.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class KediTurleri : Kedi
+    {
+        public string kediTuru ;
+
+        public KediTurleri(string _kediTuru)
+        {
+            kediTuru = _kediTuru;
+            
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Kedi> kediList = new List<Kedi>() {
+            new Kedi { kediYasi = 2, kediIsmi = "boncuk",HayvanAyakSayisi=4, },
+            new Kedi { kediYasi = 3, kediIsmi = "amavi",HayvanAyakSayisi=5, },
+            new Kedi { kediYasi = 4, kediIsmi = "blue",HayvanAyakSayisi=6, },
+            new Kedi { kediYasi = 5, kediIsmi = "boz",HayvanAyakSayisi=8, }
+            };
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
